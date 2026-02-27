8682faf3cd623fa45bde9d97898bb4246aead31e
Yash Mayekar <yashvmayekar@gmail.com>
2026-02-27 18:56:39 +0530

    test2 - 5

--- new folder/text.py
+++ new folder/text.py
@@ -21,14 +21,7 @@ def read_data(path):
     """Read and return all lines from the data file."""
     with open(path, "r") as f:
         return f.readlines()
-def read_data(path):
-    """Read and return all lines from the data file."""
-    with open(path, "r") as f:
-        return f.readlines()
-def read_data(path):
-    """Read and return all lines from the data file."""
-    with open(path, "r") as f:
-        return f.readlines()
+
 
 
 @dataclass
